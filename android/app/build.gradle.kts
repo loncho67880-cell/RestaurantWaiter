c2302bbd8bc91ed9942bb5eac7b68eefa537d7e4
German Trujillo Franco <loncho67880@gmail.com>
2026-07-02 11:33:54 -0500

    se corrige cmpilacion android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.restaurantmobile"
+    namespace = "com.example.restaurant_waiter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
